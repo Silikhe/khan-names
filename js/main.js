3ddfd7ce301b0e82621ad495d8e2293e936351d0
Silikhe <silaswans@gmail.com>
2021-06-12 21:52:30 +0300

    get the current year

--- js/main.js
+++ js/main.js
@@ -8,79 +8,67 @@ let DD;
 let YY;
 let MM;
 
-
 let akanMale = [
-    {"Sunday": "Kwasi"},
-    {"Monday": "Kwadwo"},
-    {"Tuesday": "Kwabena"},
-    {"Wednesday": "Kwaku"},
-    {"Thursday": "Yaw"},
-    {"Friday": "Kofi"},
-    {"Saturday": "Kwame"},
-]
+  { Sunday: "Kwasi" },
+  { Monday: "Kwadwo" },
+  { Tuesday: "Kwabena" },
+  { Wednesday: "Kwaku" },
+  { Thursday: "Yaw" },
+  { Friday: "Kofi" },
+  { Saturday: "Kwame" },
+];
 
 let akanFemale = [
-    {"Sunday": "Akosua"},
-    {"Monday": "Adwoa"},
-    {"Tuesday": "Abenaa"},
-    {"Wednesday": "Akua"},
-    {"Thursday": "Yaa"},
-    {"Friday": "Afua"},
-    {"Saturday": "Ama"},
-]
+  { Sunday: "Akosua" },
+  { Monday: "Adwoa" },
+  { Tuesday: "Abenaa" },
+  { Wednesday: "Akua" },
+  { Thursday: "Yaa" },
+  { Friday: "Afua" },
+  { Saturday: "Ama" },
+];
 
-document.getElementById("dateInput").addEventListener("change", function() {
-    var input = this.value;
-    var dateEntered = new Date(input);
-    console.log(input); //e.g. 2015-11-13
-    console.log(dateEntered); //e.g. Fri Nov 13 2015 00:00:00 GMT+0000 (GMT Standard Time)
+document.getElementById("dateInput").addEventListener("change", function () {
+  var input = this.value;
+  var dateEntered = new Date(input);
+  console.log(input); //e.g. 2015-11-13
+  console.log(dateEntered); //e.g. Fri Nov 13 2015 00:00:00 GMT+0000 (GMT Standard Time)
 
+  // CC - is the century digits. For example 1989 has CC = 19
+  CC = dateEntered.getFullYear();
+  CC = CC.toString().substr(0, 2);
+  console.log(CC);
 
-        // CC - is the century digits. For example 1989 has CC = 19
-        CC = dateEntered.getFullYear()
-        CC = CC.toString().substr(0, 2);
-        console.log(CC)
+  // YY - is the Year digits (1989 has YY = 89)
+  YY = dateEntered.getFullYear();
+  YY = YY.toString().substr(2, 4);
+  console.log(YY);
 
+  // MM -  is the Month
+  MM = dateEntered.getMonth();
+  console.log(MM);
 
-            // YY - is the Year digits (1989 has YY = 89)
-        YY = dateEntered.getFullYear()
-        YY = YY.toString().substr(2, 4);
-        console.log(YY)
+  // DD - is the Day of the month
 
+  DD = dateEntered.getDay();
+  console.log(DD);
 
-        MM = dateEntered.getMonth()
-        console.log(MM)
-
-
-
-
+  dayOfTheWeek();
 });
 
 // Formulae for calculating day of the month
 // (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7
 
 const dayOfTheWeek = (fullDate) => {
-   
 
-    // CC - is the century digits. For example 1989 has CC = 19
+  let firstPart = CC / 4 - 2 * CC - 1;
+  let secondPart = (5 * YY) / 4;
+  let thirdPart = (26 * (MM + 1)) / 10;
 
-    // YY - is the Year digits (1989 has YY = 89)
+  let d = (CC / 4 - 2 * CC - 1 + (5 * YY) / 4 + (26 * (MM + 1)) / 10 + DD) % 7;
 
-    // MM -  is the Month
-
-    // DD - is the Day of the month 
-
-
-    let firstPart = ( (CC/4) -2*CC-1)
-    let secondPart = ((5*YY/4) )
-    let thirdPart = ((26*(MM+1)/10))
-
-    let dayOfTheWeek = (firstPart + secondPart + thirdPart + DD) % 7
-
-
-}
+  console.log(d);
+};
 
 let date = new Date();
 // alert(date.getTimezoneOffset)
-
-
