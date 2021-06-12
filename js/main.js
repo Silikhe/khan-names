4cb0c108d3fe452f3659835609f7eab68c38610d
Silikhe <silaswans@gmail.com>
2021-06-12 21:42:38 +0300

    get the current month

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 // User birthday say 03/07/1778
 // Calculate the day of the week they were born
 // Depending on the gender give them a name
-const mydate = document.getElementById("dateInput");
+// const dDate = document.getElementById("dateInput");
 let CC;
 let DD;
 let YY;
@@ -29,11 +29,31 @@ let akanFemale = [
     {"Saturday": "Ama"},
 ]
 
-mydate.addEventListener("change", function() {
+document.getElementById("dateInput").addEventListener("change", function() {
     var input = this.value;
     var dateEntered = new Date(input);
     console.log(input); //e.g. 2015-11-13
-    console.log(dateEntered()); //e.g. Fri Nov 13 2015 00:00:00 GMT+0000 (GMT Standard Time)
+    console.log(dateEntered); //e.g. Fri Nov 13 2015 00:00:00 GMT+0000 (GMT Standard Time)
+
+
+        // CC - is the century digits. For example 1989 has CC = 19
+        CC = dateEntered.getFullYear()
+        CC = CC.toString().substr(0, 2);
+        console.log(CC)
+
+
+            // YY - is the Year digits (1989 has YY = 89)
+        YY = dateEntered.getFullYear()
+        YY = YY.toString().substr(2, 4);
+        console.log(YY)
+
+
+        MM = dateEntered.getMonth()
+        console.log(MM)
+
+
+
+
 });
 
 // Formulae for calculating day of the month
