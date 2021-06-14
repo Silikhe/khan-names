01f0a9e4451fac75439f3ebb260903cbbb36f486
Silikhe <silaswans@gmail.com>
2021-06-14 10:31:44 +0300

    Working on changes

--- js/main.js
+++ js/main.js
@@ -6,6 +6,9 @@
 
 const male = document.getElementById("maleRadio");
 const female = document.getElementById("femaleRadio");
+let title = document.getElementById("title");
+let text = document.getElementById("text");
+
 
 let CC;
 let DD;
@@ -56,6 +59,9 @@ let akanMale = [
 const getDate = () => {
   document.getElementById("dateInput").addEventListener("change", function () {
     var input = this.value;
+    if (input == null){
+      alert("Enter Date")
+    }
     var dateEntered = new Date(input);
     console.log(input); //e.g. 2015-11-13
     console.log(dateEntered); //e.g. Fri Nov 13 2015 00:00:00 GMT+0000 (GMT Standard Time)
@@ -102,33 +108,52 @@ const dayOfTheWeek = () => {
   console.log("day of the formulae " + d);
 
   // let gender = prompt("Whats your Gender?");
-  
+
   // console.log("CheckBox "+ checked)
 
   if (male.checked) {
     akanMale.find((item, index, arr) => {
       if (index == day) {
         console.log(item.ghana, item.english);
-        document.getElementById("title").innerHTML = item.ghana
-        document.getElementById("text").innerHTML = `Means an African male 👨🏿 born on ${item.english}`
-
+        title.innerHTML = item.ghana;
+        document.getElementById(
+          "text"
+        ).innerHTML = `Means an African male 👨🏿 born on ${item.english}`;
       }
     });
   } else if (female.checked) {
     akanMale.find((item, index, arr) => {
       if (index == day) {
         console.log(item.ghana, item.english);
-        document.getElementById("title").innerHTML = item.ghana
-        document.getElementById("text").innerHTML =`Means an African female 👧🏿 born on ${item.english}`
+        document.getElementById("title").innerHTML = item.ghana;
+        document.getElementById(
+          "text"
+        ).innerHTML = `Means an African female 👧🏿 born on ${item.english}`;
       }
     });
   }
 };
 
 const validate = () => {
-  if ((male.checked && female.checked) == false) {
-    alert("Gender Must set");
+  if ((((male.checked)  == false) && ((female.checked) == false)) ) {
+    document.getElementById("messageOne").innerHTML =
+      "Sellect your gender and check you date formart";
   }
+  else {
+    document.querySelector(".form").style.display = "none";
+    document.querySelector(".display").style.display = "block";
+    dayOfTheWeek();
+    // document.getElementById("message").innerHTML =
+    //   "This is a valid date format.";
+  }
+
+  let isValidDate = Date.parse("03/17/2021");
+
+  if (isNaN(isValidDate)) {
+    document.getElementById("message").innerHTML =
+      "This is not a valid date format.";
+      console.log("This is not a valid date format.")
+  } 
 };
 
 let date = new Date();
@@ -136,6 +161,80 @@ let date = new Date();
 getDate();
 
 document.getElementById("btn").addEventListener("click", () => {
-  dayOfTheWeek();
+  // dayOfTheWeek();
+  congrats()
   validate();
+  // document.querySelector(".form").style.display = "none";
+  // document.querySelector(".display").style.display = "block";
 });
+
+congrats()
+const congrats = () => {
+  // alert("congrats")
+  const Confettiful = function (el) {
+    this.el = el;
+    this.containerEl = null;
+
+    this.confettiFrequency = 3;
+    this.confettiColors = [
+      "#EF2964",
+      "#00C09D",
+      "#2D87B0",
+      "#48485E",
+      "#EFFF1D",
+    ];
+    this.confettiAnimations = ["slow", "medium", "fast"];
+
+    this._setupElements();
+    this._renderConfetti();
+  };
+
+  Confettiful.prototype._setupElements = function () {
+    const containerEl = document.createElement("div");
+    const elPosition = this.el.style.position;
+
+    if (elPosition !== "relative" || elPosition !== "absolute") {
+      this.el.style.position = "relative";
+    }
+
+    containerEl.classList.add("confetti-container");
+
+    this.el.appendChild(containerEl);
+
+    this.containerEl = containerEl;
+  };
+
+  Confettiful.prototype._renderConfetti = function () {
+    this.confettiInterval = setInterval(() => {
+      const confettiEl = document.createElement("div");
+      const confettiSize = Math.floor(Math.random() * 3) + 7 + "px";
+      const confettiBackground =
+        this.confettiColors[
+          Math.floor(Math.random() * this.confettiColors.length)
+        ];
+      const confettiLeft =
+        Math.floor(Math.random() * this.el.offsetWidth) + "px";
+      const confettiAnimation =
+        this.confettiAnimations[
+          Math.floor(Math.random() * this.confettiAnimations.length)
+        ];
+
+      confettiEl.classList.add(
+        "confetti",
+        "confetti--animation-" + confettiAnimation
+      );
+      confettiEl.style.left = confettiLeft;
+      confettiEl.style.width = confettiSize;
+      confettiEl.style.height = confettiSize;
+      confettiEl.style.backgroundColor = confettiBackground;
+
+      confettiEl.removeTimeout = setTimeout(function () {
+        confettiEl.parentNode.removeChild(confettiEl);
+      }, 3000);
+
+      this.containerEl.appendChild(confettiEl);
+    }, 25);
+  };
+
+  window.confettiful = new Confettiful(document.querySelector(".js-container"));
+};
